38048b0c26bb26f07894148b325670f690c4b2ef
ben-jeffrey <ben.jeffrey@maine.edu>
2020-04-25 22:59:40 -0400

    consolidation

--- plugins/Degenesis.js
+++ plugins/Degenesis.js
@@ -5,16 +5,14 @@ exports.command = function(userID, channelID, serverID, message, sender) {
         message += 'd6>3';
         let arg = Common.args(message)[0];
         let roller = create_roller(arg, serverID, userID);
-        let botch = calcBotch(roller) ? ' (botched if failed)' : ''
-        let triggers = roller.roll_list.filter(r => r === 6).length;
-        sender(channelID, roller.output()+` (${triggers} triggers)`+botch, userID)
+        sender(channelID, roller.output()+calcTriggers(roller)+calcBotch(roller), userID)
     } else if (message.match(/^\-rr \d+ \d+$/i)) {
         let text = Common.args(message)[0] + 'd6>3' || '';
         let count = Common.args(message)[1] || 1;
         let roller = create_roller(text, serverID, userID);
         let results = [];
         for (let i = 0; i < count; i++)
-            results.push(roller.roll().output()+` (${roller.roll_list.filter(r => r === 6).length} triggers)`+(calcBotch(roller) ? ' (botched if failed)\n' : '\n'));
+            results.push(roller.roll().output()+calcTriggers(roller)+calcBotch(roller)+'\n');
         sender(channelID, results, userID)
     } else return true;
 }
@@ -22,7 +20,12 @@ exports.command = function(userID, channelID, serverID, message, sender) {
 function calcBotch(roller) {
     let ones = roller.roll_list.filter(r => r === 1).length;
     let successes = roller.roll_list.filter(r => r > 3).length;
-    return ones > successes;
+    return ones > successes ? ' (botched if failed)' : ''
+}
+
+function calcTriggers(roller) {
+    let count = roller.roll_list.filter(r => r === 6).length;
+    return count > 0 ? count > 1 ? ` (${count} triggers)` : '1 trigger' : '';
 }
 
 function create_roller(text, server, user) {
